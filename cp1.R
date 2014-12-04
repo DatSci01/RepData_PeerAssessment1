b67a426badbc781527616cbccba25d6b576f868b
BCottingham <BCottingham50@gmail.com>
2014-12-03 22:57:18 -0600

    Add remarks

--- cp1.R
+++ cp1.R
@@ -1,9 +1,15 @@
 unzip("activity.zip")
 activity<-read.csv("activity.csv")
+
+## aggregate total steps by date
 tots<-aggregate(steps~date,data=activity,sum)
+
+## produce histogram
 hist(tots$steps)
 #aves<-aggregate(steps~date,data=activity,mean)
 #print(aves)
+
+## calculate mean and median of total steps (tots)
 meanSteps<-mean(tots$steps)
 medianSteps<-median(tots$steps)
 print(paste("Mean total steps per day: ",meanSteps))
